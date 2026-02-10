974645a957f13d912a63ffd19fd59c02b8487e25
Gabriel Gonnzález <gabrielgg2005ve@gmail.com>
2026-02-10 15:04:46 -0400

    Fix and Update(Footer.astro TopNavbar.astro index.astro astro.config.mjs SideNavbar.astro)  Se actualizo y arreglo los errores con los imports de iconos, la configuracion en astro.config.mjs. Se adapto el navbar top y el navbar side para mejor experiencia usuario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        tabler: ["*"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
