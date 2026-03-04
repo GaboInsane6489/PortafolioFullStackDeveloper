6cc91a17913f1cfe9dd14c72d5bad100b489f960
Gabriel Gonnzález <gabrielgg2005ve@gmail.com>
2026-03-04 16:39:05 -0400

    feat(seo): update og:image and twitter:image to use Hero screenshot

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
+  site: "https://portafolio-full-stack-developer-pink.vercel.app",
   integrations: [
     icon({
       include: {
         tabler: ["*"],
       },
     }),
+    sitemap(),
   ],
   vite: {
     plugins: [tailwindcss()],
